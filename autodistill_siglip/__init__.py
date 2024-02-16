7243f6e3ff3be1be315e1f92b206b5e0bd9fd802
James Gallagher <jamesg@jamesg.blog>
2024-02-16 22:03:37 +0000

    add siglip model, remove unused files

---
+++ autodistill_siglip/__init__.py
@@ -0,0 +1,3 @@
+from autodistill_siglip.siglip_model import SigLIP
+
+__version__ = "0.1.0"
---
+++ autodistill_siglip/siglip_model.py
@@ -0,0 +1,37 @@
+import os
+from dataclasses import dataclass
+
+import torch
+
+import supervision as sv
+from autodistill.detection import CaptionOntology, DetectionBaseModel
+from autodistill.helpers import load_image
+import numpy as np
+from transformers import pipeline
+
+HOME = os.path.expanduser("~")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# inference
+
+@dataclass
+class SigLIP(DetectionBaseModel):
+    ontology: CaptionOntology
+    
+    def __init__(self, ontology: CaptionOntology):
+        self.model = pipeline(task="zero-shot-image-classification", model="google/siglip-base-patch16-224")
+        self.ontology = ontology
+
+    def predict(self, input: str, confidence: int = 0.5) -> sv.Classifications:
+        prompts = self.ontology.prompts()
+        outputs = self.model(load_image(input), candidate_labels=prompts)
+        outputs = [{"score": round(output["score"], 4), "label": prompts.index(output["label"]) } for output in outputs]
+
+        results = sv.Classifications(
+            class_id=np.array([output["label"] for output in outputs]),
+            confidence=np.array([output["score"] for output in outputs]),
+        )
+
+        results = results[results.confidence > confidence]
+
+        return results
